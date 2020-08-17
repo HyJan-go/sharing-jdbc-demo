1f7d9f2255933ea06cd3d4f9d6fc5d62b377c126
HyJan <hejian@52tt.com>
2020-08-17 19:42:54 +0800

    doc

--- src/main/java/com/db/sharingjdbcdemo/SharingJdbcDemoApplication.java
+++ src/main/java/com/db/sharingjdbcdemo/SharingJdbcDemoApplication.java
@@ -2,12 +2,13 @@ package com.db.sharingjdbcdemo;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 雪花算法的缺陷：如果时间不连续进行访问，很容易出现全是单数，或者全是双数的这种会造成数据都在一个表中
  * 高并发场景才适用雪花算法
+ *
+ * 缺陷：感觉数据加密并没有配错，但是数据脱敏并没有成功，当然，这个也可以通过业务层手动进行加密，看选择。
  */
 @MapperScan(basePackages = {"com.db.sharingjdbcdemo.model.mapper"})
 @SpringBootApplication
